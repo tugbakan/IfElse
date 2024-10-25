de6a13e4e0d0bff8b84b3c27147b7ba82e6bfa85
Tugba Kan <ttugbakan@gmail.com>
2024-10-25 10:57:26 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Saate göre selamlama 
+
+    int time=DateTime.Now.Hour;
+
+
+    if(time>=6 && time < 12)
+    {
+        Console.WriteLine("Günaydın");
+    }
+    else if(time<=18)
+    {
+        Console.WriteLine("İyi Günler");
+    }
+    else
+    {
+        Console.WriteLine("İyi akşamlar");
+    }
+
+    //Ternary 
+    
+    string sonuc = time <=18 ? "İyi Günler" : "İyi Akşamlar";
+    sonuc= time >= 6 && time < 12 ? "Günaydın!" : time <=18 ? "İyi Günler":"İyi Akşamlar";
+
+    Console.WriteLine(sonuc);
